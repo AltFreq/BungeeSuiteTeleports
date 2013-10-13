2ce6f7976ff9c7112661b1b47fd2e7ee6e5ce5b3
bloodsplat <pure__instinct@hotmail.com>
2013-10-13 15:28:17 +1100

    Fix #1 Removed the socket

--- src/main/java/com/minecraftdimensions/bungeesuiteteleports/tasks/PluginMessageTask.java
+++ src/main/java/com/minecraftdimensions/bungeesuiteteleports/tasks/PluginMessageTask.java
@@ -1,68 +1,32 @@
 package com.minecraftdimensions.bungeesuiteteleports.tasks;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.minecraftdimensions.bungeesuiteteleports.BungeeSuiteTeleports;
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import com.minecraftdimensions.bungeesuiteteleports.BungeeSuiteTeleports;
-import com.minecraftdimensions.bungeesuiteteleports.socket.Client;
+import java.io.ByteArrayOutputStream;
 
 public class PluginMessageTask extends BukkitRunnable {
 
-	private final ByteArrayOutputStream bytes;
-	private final boolean empty;
+    private final ByteArrayOutputStream bytes;
 
-	public PluginMessageTask(ByteArrayOutputStream bytes) {
-		this.bytes = bytes;
-		empty = false;
-	}
+    public PluginMessageTask( ByteArrayOutputStream bytes ) {
+        this.bytes = bytes;
+    }
 
-	public PluginMessageTask(ByteArrayOutputStream b, boolean empty) {
-		this.bytes = b;
-		this.empty = empty;
-	}
+    public PluginMessageTask( ByteArrayOutputStream b, boolean empty ) {
+        this.bytes = b;
+    }
 
-	@SuppressWarnings("unchecked")
-	public void run() {
-		if (!empty) {
-			Player p = Bukkit.getOnlinePlayers()[0];
-			if(p==null){
-				return;
-			}
-			p.sendPluginMessage(
-					BungeeSuiteTeleports.instance,
-					BungeeSuiteTeleports.OUTGOING_PLUGIN_CHANNEL,
-					bytes.toByteArray());
-		} else {
-			File customConfigFile = new File("spigot.yml");
-			YamlConfiguration customConfig = YamlConfiguration
-					.loadConfiguration(customConfigFile);
-			InputStream defConfigStream = BungeeSuiteTeleports.instance
-					.getResource("spigot.yml");
-			if (defConfigStream != null) {
-				YamlConfiguration defConfig = YamlConfiguration
-						.loadConfiguration(defConfigStream);
-				customConfig.setDefaults(defConfig);
-			}
-			List<String> bungee;
-			if (customConfig != null) {
-				bungee = (List<String>) customConfig
-						.getList("settings.bungeecord-addresses");
-			} else {
-				bungee = new ArrayList<String>();
-				bungee.add("127.0.0.1");
-			}
-			for (String l : bungee) {
-				Client.send(l, 14455, bytes);
-			}
-		}
-	}
+    @SuppressWarnings("unchecked")
+    public void run() {
+        Player p = Bukkit.getOnlinePlayers()[0];
+        if ( p == null ) {
+            return;
+        }
+        p.sendPluginMessage( BungeeSuiteTeleports.instance, BungeeSuiteTeleports.OUTGOING_PLUGIN_CHANNEL, bytes.toByteArray() );
+    }
+
 
 }
