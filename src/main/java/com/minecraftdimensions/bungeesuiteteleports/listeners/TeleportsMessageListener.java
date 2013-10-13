d3589ddb3d4f8b8134401781dc5de73aa6e45fdd
bloodsplat <pure__instinct@hotmail.com>
2013-10-13 17:08:26 +1100

    Add /bsversion support to all modules

--- src/main/java/com/minecraftdimensions/bungeesuiteteleports/listeners/TeleportsMessageListener.java
+++ src/main/java/com/minecraftdimensions/bungeesuiteteleports/listeners/TeleportsMessageListener.java
@@ -1,39 +1,44 @@
 package com.minecraftdimensions.bungeesuiteteleports.listeners;
 
+import com.minecraftdimensions.bungeesuiteteleports.BungeeSuiteTeleports;
+import com.minecraftdimensions.bungeesuiteteleports.managers.TeleportsManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.messaging.PluginMessageListener;
-
-import com.minecraftdimensions.bungeesuiteteleports.managers.TeleportsManager;
-
 public class TeleportsMessageListener implements PluginMessageListener {
 
-	@Override
-	public void onPluginMessageReceived(String channel, Player player,
-			byte[] message) {
-		DataInputStream in = new DataInputStream(new ByteArrayInputStream(
-				message));
-		String task = null;
-		try {
-			task = in.readUTF();
-		if(task.equals("TeleportToPlayer")){
-			TeleportsManager.teleportPlayerToPlayer(in.readUTF(), in.readUTF());
-		}
-		
-		if(task.equals("TeleportToLocation")){
-			String name = in.readUTF();
-			String loc = in.readUTF();
-			String locs [] = loc.split("~!~");
-			TeleportsManager.teleportPlayerToLocation(name, locs[1], Double.parseDouble(locs[2]),Double.parseDouble(locs[3]),Double.parseDouble(locs[4]));
-		}
-		
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    @Override
+    public void onPluginMessageReceived( String channel, Player player, byte[] message ) {
+        DataInputStream in = new DataInputStream( new ByteArrayInputStream( message ) );
+        String task;
+        try {
+            task = in.readUTF();
+            if ( task.equals( "TeleportToPlayer" ) ) {
+                TeleportsManager.teleportPlayerToPlayer( in.readUTF(), in.readUTF() );
+            }
 
-	}
+            if ( task.equals( "TeleportToLocation" ) ) {
+                String name = in.readUTF();
+                String loc = in.readUTF();
+                String locs[] = loc.split( "~!~" );
+                TeleportsManager.teleportPlayerToLocation( name, locs[1], Double.parseDouble( locs[2] ), Double.parseDouble( locs[3] ), Double.parseDouble( locs[4] ) );
+            }
+            if ( task.equals( "GetVersion" ) ) {
+                Player p = Bukkit.getPlayer( in.readUTF() );
+                p.sendMessage( ChatColor.RED + "Teleports - " + ChatColor.GOLD + BungeeSuiteTeleports.instance.getDescription().getVersion() );
+                Bukkit.getConsoleSender().sendMessage( ChatColor.RED + "Teleports - " + ChatColor.GOLD + BungeeSuiteTeleports.instance.getDescription().getVersion() );
+            }
+
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
